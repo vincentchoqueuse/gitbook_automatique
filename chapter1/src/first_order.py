664753e8406e121235811930c762dccb34d134d8
Vincent Choqueuse <vincent.choqueuse@gmail.com>
2017-11-26 10:47:39 +0100

    truc

--- chapter1/src/first_order.py
+++ chapter1/src/first_order.py
@@ -6,9 +6,9 @@ K=2
 tau=3
 E=4
 
-lti=signal.lti([K*E],[tau,1])
+lti=signal.lti([K],[tau,1])
 t,y=lti.step()
-plt.plot(t,y)
+plt.plot(t,E*y)
 plt.plot([0,25],[K*E,K*E],'r--')
 plt.plot([0,3*tau],[0.95*K*E,0.95*K*E],'r--')
 plt.plot([3*tau,3*tau],[0,0.95*K*E],'r--')
@@ -26,5 +26,29 @@ plt.xlabel("temps (s)")
 plt.ylabel("sortie")
 plt.ylim([0,9])
 plt.xlim([0,20])
+plt.savefig("../fig1.png")
+
+
+w2=np.linspace(1/tau, 10, num=100)
+plt.figure()
+ax = plt.gca()
+w,G,phase=lti.bode(w=np.logspace(-2, 1, num=100))
+plt.semilogx(w,G)
+plt.plot([0.01,10],[20*np.log10(K),20*np.log10(K)],'r--')
+plt.plot([1/tau,1/tau],[-25,6],'r--')
+plt.plot([0.01,1/tau],[20*np.log10(K)-3,20*np.log10(K)-3],'r--')
+
+
+plt.plot(w2,20*np.log10(K)-20*np.log10(w2*tau),'r--')
+plt.xlabel("pulsation (rad/s)")
+plt.ylabel("Gain (dB)")
+ax.annotate('Basse Fréquence: 20.log10(K)', xy=(0.01, 20*np.log10(K)), xytext=(0.03,8),color='red',
+            arrowprops=dict(width=1,color='red', shrink=0.005),
+            )
+ax.annotate('20.log10(K)-3', xy=(0.01, 20*np.log10(K)-3), xytext=(0.03,-5),color='red',
+            arrowprops=dict(width=1,color='red', shrink=0.005),
+            )
+
+ax.text(1/tau-0.05, -26.5,'$1/\\tau$',color='red')
 plt.savefig("../fig1.png")
 plt.show()
